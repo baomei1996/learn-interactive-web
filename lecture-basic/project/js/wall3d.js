d4da88caeba7efbd54b2bb56da86db16d7d74dc3
BomiAn <qhal4686@gmail.com>
2022-04-30 23:56:33 +0900

    project 3D wall (4)

--- lecture-basic/project/js/wall3d.js
+++ lecture-basic/project/js/wall3d.js
@@ -1,12 +1,22 @@
 (function () {
   const houseElem = document.querySelector('.house');
+  const barElem = document.querySelector('.progress-bar')
   let maxScrollValue = document.body.offsetHeight  - window.innerHeight
-
   window.addEventListener('scroll', function() {
     //console.log(this.pageYOffset);
     // console.log( pageYOffset / maxScrollValue);
-    const zMove = pageYOffset / maxScrollValue * 980 - 490;
 
+    // z scroll
+    const scrollPer = pageYOffset / maxScrollValue
+    const zMove =  scrollPer * 980 - 490;
     houseElem.style.transform = 'translateZ(' + zMove + 'vw)'
+
+    // progress bar
+    barElem.style.width = scrollPer * 100 + '%'
   })
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight  - window.innerHeight
+  }
+
+  window.addEventListener('resize', resizeHandler)
 })();
